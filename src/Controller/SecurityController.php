fb81b968390bd68b9f1b9f2e15ee15613c65de16
blancho <blanchard.banyingela@gmail.com>
2024-02-22 19:15:39 +0100

    Finalisation connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bridge\Twig\NodeVisitor\Scope;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
+    public const SCOPES = [
+        'google' => [],
+        'github' => ['user:email'],
+        'facebook' => [],
+    ];
+
+
+    #[Route(path: '/login', name: 'app_login',  methods:['GET'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
         if ( $this->getUser() ) {
@@ -29,4 +41,27 @@ class SecurityController extends AbstractController
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
+
+    #[Route(path: '/oauth/connect/service/{service}', name: 'app_oauth_login',  methods:['GET'])]
+    public function connect(string $service, ClientRegistry $clientRegistry): RedirectResponse
+    {
+        if ( !in_array($service, array_keys(self::SCOPES), TRUE) )
+        {
+            throw $this->createNotFoundException() ;
+        }
+
+        // $clientRegistry = $this->get('knpu.oauth2.registry'); 
+        return $clientRegistry
+            ->getClient($service) // the name use in config/packages/knpu_oauth2_client.yaml 
+            ->redirect( self::SCOPES[$service], []) ;  // 'public_profile', 'email' ,  the scopes you want to access
+    }
+
+    #[Route('/connexion/service/check', name: 'connect_check',  methods:['GET', 'POST'])]
+    public function connectCheckAction(): Response
+    {
+       
+            return $this->redirectToRoute('app_home') ;
+    
+    }
+
 }
