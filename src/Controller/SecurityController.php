590b174af3ec2f94b771ac85b1df0892af7d96c6
blancho <blanchard.banyingela@gmail.com>
2024-02-26 17:48:47 +0100

    refacto

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -59,11 +59,7 @@ class SecurityController extends AbstractController
     #[Route('/oauth/check/{service}', name: 'auth_oauth_check',  methods:['GET','POST'])]
     public function connectCheckAction( Request $request, ClientRegistry $clientRegistry): Response
     {
-       // $service =  $request->attributes->all()['service'];
-       $service = $request->get(key: 'service') ;
-       
-       dd('test') ;
-       dd($service) ;
+        $service =  $request->attributes->all()['service'];
        
         if ( !in_array($service, array_keys(self::SCOPES), TRUE) )
         {
